0ac7954c1ecf00424af842885fd561636efbf412
maxalanclark <max.a.clark@gmail.com>
2025-02-14 16:51:07 -0800

    - Brought in print7x from unity

--- WorkoutPrinter.js
+++ WorkoutPrinter.js
@@ -47,3 +47,39 @@ function getRandomExercises(exerciseList, num) {
     let shuffled = [...exerciseList].sort(() => 0.5 - Math.random());
     return shuffled.slice(0, num);
 }
+
+
+/*
+
+tatic void Print7x()
+    {
+        List<string> arms = new List<string>() {"Pushup Pull Throughs", "Flappy Birds", "Plank Pushups", "Tricep Dips", "Standing Triceps", "Burpees", "Burpees"};
+        List<string> abs = new List<string>()  {"Crunchy Frogs", "Fifer Scissors", "Leg Lifts", "Reverse Crunches", "Bicycles", "Roll Up V Ups", "Russian Twists"};
+        List<string> legs = new List<string>() {"Kettlebell Swings", "Lunge Uppercuts", "Jump Lunges", "Jump Squats", "Mountain Climbers", "Squat Shuffle", "Stir the Pot"};
+
+        string s = "";
+        for (int i = 0; i < 7; i++)
+        {
+            s += "set: " + (i + 1) + System.Environment.NewLine;
+            string a = arms[UnityEngine.Random.Range(0, arms.Count)];
+            arms.Remove(a);
+            string b = abs[UnityEngine.Random.Range(0, abs.Count)];
+            abs.Remove(b);
+            string c = legs[UnityEngine.Random.Range(0, legs.Count)];
+            legs.Remove(c);
+
+            List<string> set = new List<string>() {a, b, c};
+            set.Shuffle();
+            for (int j = 0; j < set.Count; j++)
+            {
+                s += set[j];
+                s += System.Environment.NewLine;
+            }
+            if (i < 6) s+= System.Environment.NewLine;
+        }
+
+        s += "\n maybe consider stretching afterwards"; 
+        Debug.Log(s);
+    }
+
+    */
